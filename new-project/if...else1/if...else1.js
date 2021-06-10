97968227e1d0b30b229ad20db39e61ec28654983
Rentalas <lozrak@gmail.com>
2021-06-10 21:39:39 +0300

    welcome back

---
+++ new-project/if...else1/if...else1.js
@@ -0,0 +1,14 @@
+let answer=prompt('put it here', 0);
+
+if (answer>0) 
+{
+alert(1);
+}
+else if(answer<0)
+{
+alert(-1)
+}
+else 
+{
+alert(0)
+}
---
+++ new-project/if..else2+и или не)/и или.js
@@ -0,0 +1,3 @@
+let age = prompt('How old are you?');
+if (age>=14 && age<=90) alert('you are so small')
+else alert('You are so big');
